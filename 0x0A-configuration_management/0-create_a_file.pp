08906398c19f6f4460768a8314fc70c82175ecac
musaubrian <musaubrian15@gmail.com>
2022-09-18 13:18:29 +0300

    update: fix indentation error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #creates a file with permissions and groups
 
 file {'/tmp/school':
-content => 'I love puppet',
-owner => 'www-data',
-group => 'www-data',
-mode => '0744',
+  content => 'I love puppet',
+  owner => 'www-data',
+  group => 'www-data',
+  mode => '0744',
 }
