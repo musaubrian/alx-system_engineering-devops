a6dcac00ea23bb89595cd8ba0217f3fa32032324
musaubrian <musaubrian45@gmail.com>
2022-09-18 13:15:04 +0300

    update: remove recursive

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,8 @@
 #creates a file with permissions and groups
 
 file {'/tmp/school':
-    content => 'I love puppet'
-    recursive => false,
-    owner => 'www-data',
-    group => 'www-data',
-    mode => 0744,
-    
-
+content => 'I love puppet',
+owner => 'www-data',
+group => 'www-data',
+mode => '0744',
 }
