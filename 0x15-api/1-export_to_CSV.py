77fc50f9069281f536c3e7383b976c47856002bc
musaubrian <musaubrian15@gmail.com>
2022-10-17 17:00:32 +0300

    Chore: write contents to csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+send request to an API
+"""
+
+import csv
+import requests
+import sys
+
+
+def send_request():
+    """
+    Sends a request for a json file
+    """
+    employee_id = sys.argv[1]
+    user_response = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+            )
+    todos_response = requests.get(
+            "https://jsonplaceholder.typicode.com/todos/"
+            )
+
+    employee_name = user_response.json().get('name')
+    
+    filename = employee_id + '.csv'
+    with open(filename, 'w') as file:
+        csv_writer = csv.writer(file, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos_response.json():
+            if task.get('userId') == int(employee_id):
+                csv_writer.writerow([employee_id, employee_name, str(task.get('completed')),
+                                 task.get('title')])
+
+if __name__ == "__main__":
+    send_request()
