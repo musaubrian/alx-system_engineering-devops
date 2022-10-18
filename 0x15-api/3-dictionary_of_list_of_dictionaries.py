a449a8f427eaeb8f1d5d4a65b0391e298a86ae50
musaubrian <musaubrian15@gmail.com>
2022-10-18 16:05:09 +0300

    Chore: get all the users and their tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,2 +1,33 @@
 #!/usr/bin/python3
-"""module returns a """
+"""module returns all the tasks and saves to json file"""
+import json
+import requests
+
+
+if __name__ == "__main__":
+
+    users_response = requests.get(
+            "https://jsonplaceholder.typicode.com/users"
+            )
+    todos_response = requests.get(
+            "https://jsonplaceholder.typicode.com/todos"
+            )
+
+    users = users_response.json()
+    todos = todos_response.json()
+
+    task_lists = []
+    all_todos = {}
+
+    for user in users:
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                task_dict = {"username": user.get('username'),
+                             "task": task.get('title'),
+                             "completed": task.get('completed')
+                             }
+                task_lists.append(task_dict)
+            all_todos[user.get('id')] = task_lists
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(all_todos, file)
