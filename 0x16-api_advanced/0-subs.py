2146db50a764c7f98d2c5bc26f625d58cabd5a95
musaubrian <musaubrian15@gmail.com>
2022-11-01 13:13:13 +0300

    chore: get subscribers under a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""returns number of users in a subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    get number of subscribers
+    if subreddit is not valid return 0
+
+    Args::
+        subreddit
+    """
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
+
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 404:
+        return 0
+    
+    results = response.json().get("data")
+    return results.get("subscribers")
+        
